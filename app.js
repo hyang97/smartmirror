81432410f63b13365b71b895802c036c22c6e7a2
Bryan Wong <bryanw0ng@ucla.edu>
2018-11-28 10:55:14 -0800

    fix import

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const WebSocketServer = require("ws").server;
+const WebSocketServer = require("ws").Server;
 
 const indexRouter = require("./routes/index");
 const calendarRouter = require("./routes/calendar");
